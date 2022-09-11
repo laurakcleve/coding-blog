1144c2316e881fc40b5dffaa4c1fd054e8becc42
Laura <laurakcleve@gmail.com>
2022-09-11 14:34:01 -0600

    Add first own post

---
+++ content/blog/rounding/index.md
@@ -0,0 +1,15 @@
+---
+title: Rounding
+date: '2022-09-11'
+description: 'Description'
+---
+
+Here is a picture.
+
+![A picture](./images/pic.png)
+
+And some code.
+
+```js
+console.log('woof')
+```
--- src/components/bio.js
+++ src/components/bio.js
@@ -45,7 +45,7 @@ const Bio = () => {
       {author?.name && (
         <>
           <p>
-            Written by <strong>{author.name}</strong>, {author?.summary || null}
+            Written by <strong>{author.name}</strong>
           </p>
         </>
       )}
